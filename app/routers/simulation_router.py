47918c3c648d505835ed0dddc1bc44dc1e28272f
BarBenEzra1 <barbenezra897@gmail.com>
2025-08-15 00:28:27 +0300

    download json result

--- app/routers/simulation_router.py
+++ app/routers/simulation_router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status
+from datetime import datetime, timezone
+from fastapi import APIRouter, status, Response
 from app.domain.schemas.simulation_schemas import SimulationRequest, SimulationResponse
 from app.domain.services.simulation_service import run_simulation_by_system_id
 
@@ -6,8 +7,13 @@ from app.domain.services.simulation_service import run_simulation_by_system_id
 router = APIRouter(prefix="/simulation", tags=["Simulation"])
 
 
-@router.post("/", response_model=SimulationResponse, status_code=status.HTTP_200_OK)
+@router.post("/", status_code=status.HTTP_200_OK)
 async def run_simulation(simulation_request: SimulationRequest):
     """Run simulation"""
     
-    return run_simulation_by_system_id(simulation_request.system_id)
+    simulation_response = run_simulation_by_system_id(simulation_request.system_id)
+    json_content = simulation_response.model_dump_json(indent=2)
+    
+    filename = f"simulation_result_{datetime.now(timezone.utc).strftime('%Y-%m-%d_%H-%M-%S')}.json"
+    headers = {"Content-Disposition": f'attachment; filename="{filename}"'}
+    return Response(content=json_content, media_type="application/json", headers=headers)
