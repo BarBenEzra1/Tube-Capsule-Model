dcfe7b0949d4e3d6e3aa440ffef4fb69a0a2e34c
BarBenEzra1 <barbenezra897@gmail.com>
2025-08-15 16:15:08 +0300

    compressed json with gzip

--- app/routers/simulation_router.py
+++ app/routers/simulation_router.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timezone
 from fastapi import APIRouter, status, Response
-from app.domain.schemas.simulation_schemas import SimulationRequest, SimulationResponse
+from app.domain.schemas.simulation_schemas import SimulationRequest
 from app.domain.services.simulation_service import run_simulation_by_system_id
+import gzip
 
 
 router = APIRouter(prefix="/simulation", tags=["Simulation"])
@@ -9,11 +10,17 @@ router = APIRouter(prefix="/simulation", tags=["Simulation"])
 
 @router.post("/", status_code=status.HTTP_200_OK)
 async def run_simulation(simulation_request: SimulationRequest):
-    """Run simulation"""
+    """Run simulation and download results as JSON (optionally compressed)"""
     
     simulation_response = run_simulation_by_system_id(simulation_request.system_id)
     json_content = simulation_response.model_dump_json(indent=2)
     
-    filename = f"simulation_result_{datetime.now(timezone.utc).strftime('%Y-%m-%d_%H-%M-%S')}.json"
-    headers = {"Content-Disposition": f'attachment; filename="{filename}"'}
-    return Response(content=json_content, media_type="application/json", headers=headers)
+    timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%d_%H-%M-%S')
+    
+    compressed_content = gzip.compress(json_content.encode('utf-8'))
+    filename = f"simulation_result_{timestamp}.json.gz"
+    headers = {
+        "Content-Disposition": f'attachment; filename="{filename}"',
+        "Content-Type": "application/gzip"
+    }
+    return Response(content=compressed_content, media_type="application/gzip", headers=headers)
